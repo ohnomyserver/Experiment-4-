03132c7dc89423cf737f9e1bc2cc5e45224df8d8
ohnomyserver <harshagrawal0586@gmail.com>
2026-02-09 12:11:33 +0530

    Calling functions

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #Calculator
+
+
+import addition as a 
+import subtraction as s 
+import multiplication as m 
+
+print("Addition Result = " , a.add(5,4) ) 
+print("Subtraction Result = " , s.sub(9,5) ) 
+print("Multiplication Result = ", m.mul(5,5) ) 
+
+
