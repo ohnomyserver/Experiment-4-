9a8f6e2bd7f0cc8ed9d01f7dc5e63bfb4abe7537
ohnomyserver <harshagrawal0586@gmail.com>
2026-02-09 12:48:08 +0530

    Add function calls for arithmetic operations

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import addition as a
 import subtraction as s 
 import multiplication as m 
 
+# Calling Functions: 
 print("Addition Result = " , a.add(5,4) ) 
 print("Subtraction Result = " , s.sub(9,5) ) 
 print("Multiplication Result = ", m.mul(5,5) ) 
