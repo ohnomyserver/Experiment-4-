124069f13eddd8c2bbc18736e4a9b6238230c395
ohnomyserver <harshagrawal0586@gmail.com>
2026-02-09 13:01:44 +0530

    Calling functions

--- main.py
+++ main.py
@@ -4,10 +4,13 @@
 import addition as a 
 import subtraction as s 
 import multiplication as m 
+import division as d 
+
 
 # Calling Functions: 
 print("Addition Result = " , a.add(5,4) ) 
 print("Subtraction Result = " , s.sub(9,5) ) 
 print("Multiplication Result = ", m.mul(5,5) ) 
+print("Division Result = " , d.div (12, 4) ) 
 
 
